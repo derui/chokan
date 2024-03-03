f55eb7a599e5b51315fb9748236f2e07ba1cf8ca
derui <derutakayu@gmail.com>
2024-03-03 20:20:31 +0900

    feat: add library for trie

---
+++ trie/src/lib.rs
@@ -0,0 +1,177 @@
+/// Trieのデータ型と操作を提供するmodule
+mod types;
+
+use std::collections::HashSet;
+
+use types::{Base, Check, Empties, Label, Labels, Node, NodeIdx};
+
+/// 最も基本的なtrie構造を表現する
+/// 任意のデータ構造を保有する必要がある場合は、[HoldableTrie<T>]を利用すること
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct Trie {
+    /// ダブル配列を構成するための基本的なデータ構造。
+    nodes: Vec<Node>,
+
+    /// 内部で値として利用する、文字とデータIDのマッピングを管理する
+    /// 今回は255種類のみ許容する
+    labels: Labels,
+}
+
+impl Trie {
+    /// 新しいTrieを生成する
+    pub fn from_keys(keys: &Vec<char>) -> Trie {
+        let labels = Labels::from_keys(keys);
+
+        let mut nodes = Vec::new();
+        // 仕組み上、0のcheckが利用されることはないため、初期化時点では常に1を指しておく
+        nodes.push(Node {
+            base: Base::root(),
+            check: Check::root(),
+        });
+
+        let value_len = labels.len();
+        Empties::expand_empties(&mut nodes, value_len);
+
+        Trie { nodes, labels }
+    }
+
+    /// checkを変更する。
+    ///
+    /// checkの変更では、対象が未使用である場合には、一つ前のindexが、更新しようとしているcheckが指している
+    /// indexを指すようにしなければならない
+    fn write_check(&mut self, _base: i32, _label: i32) {}
+
+    /// nodeから遷移する先のラベルを挿入する
+    ///
+    /// ラベル集合が挿入できる場所に挿入するが、挿入できない場合は新しい領域を確保する。
+    /// この処理では、衝突回避は行わず、挿入と拡張のみを行う。
+    ///
+    /// # Arguments
+    /// - `node` - 現在注目しているnode
+    /// - `base` - baseとして設定する値
+    /// - `labels` - 遷移先ラベルの集合
+    ///
+    fn write_labels(&mut self, _node: usize, _base: i32, _labels: &HashSet<i32>) {}
+
+    /// xcheckアルゴリズムの実装。
+    ///
+    /// # Arguments
+    /// - `labels` - 遷移先ラベルの集合
+    ///
+    /// # Returns
+    /// 返却される値は、すべてのlabelが設定可能と判定されたbaseのindexである
+    fn xcheck(&self, labels: &Vec<Label>) -> Base {
+        let ary_size = self.nodes.len() as u32;
+        let min_label = labels.iter().min().unwrap();
+        let other_labels: Vec<Label> = labels.iter().filter(|v| *v == min_label).cloned().collect();
+
+        for e in Empties::as_iter(&self.nodes) {
+            // 実際に空として認識するのは、最小のlabelのみが基準となる
+            if let Some(t) = e - *min_label {
+                let mut is_ok = true;
+
+                // 一つでも利用中のcheckがある場合は、何もしない
+                for label in other_labels.iter() {
+                    let i = t + *label;
+                    if self.nodes[usize::from(i)].check.is_used() {
+                        is_ok = false;
+                        break;
+                    }
+                }
+
+                if is_ok {
+                    // 見つけたbaseを利用する。
+                    return t;
+                }
+            }
+        }
+
+        // 一切見つからなかった場合は、新しい領域を追加する必要がある
+        Base::new(ary_size)
+    }
+
+    /// 指定したnodeから参照しているcheckを未使用状態に戻す
+    fn clear(&mut self, _node_idx: &i32, _new_base: &i32) {}
+
+    /// 指定されているbaseとlabelから、衝突しているbaseの移動を行う
+    ///
+    /// 此処で移動対象になる木は、labelの数が少ない方を優先する。
+    ///
+    /// # Arguments
+    /// - `node` - 現在注目しているnode。baseを取得したもとのindexになる
+    /// - `check` - 設定先のcheck
+    /// - `label` - 対象のlabel
+    ///
+    fn modify(&mut self, _node: &i32, _check_idx: &i32, _label: &i32) {
+        unimplemented!();
+        // let check_node = self.nodes[*check_idx as usize];
+
+        // assert!(
+        //     check_node.base != dbary::calc_base(*check_idx, *label),
+        //     "should be different base"
+        // );
+
+        // let current_base = dbary::calc_base(*check_idx, *label);
+        // let mut current_labels = self.get_labels_of(current_base);
+        // let mut other_labels = self.get_labels_of(check_node.base);
+
+        // // 対象も含めて移動することになるので、ここのタイミングで追加しておく
+        // current_labels.insert(*label);
+
+        // // それぞれ新しい場所に書き込むのだが、移動した方は、元々の場所を初期化しなければならない
+        // if current_labels.len() < other_labels.len() {
+        //     let new_base = self.xcheck(&current_labels);
+        //     self.write_labels(*node, new_base, &current_labels);
+        //     self.clear(node, &new_base);
+        // } else {
+        //     // 元々あった方を動かす場合は、baseがcheckが指している場所になる。
+        //     let new_base = self.xcheck(&other_labels);
+        //     self.write_labels(*check_node, new_base, &other_labels);
+        //     self.clear(&current_base, &new_base);
+        // }
+    }
+
+    /// Trieに新しいキーを追加する
+    ///
+    /// 追加は、以下のアルゴリズムで行う。
+    /// 1. キーをlabelに変換する
+    /// 2. 先頭のlabelから、baseを計算する
+    /// 3. 計算したbaseから遷移先を取得する
+    /// 4. 今回追加するlabelが衝突していないかcheckする
+    /// 5. 衝突していない場合、label含めて対応するbaseを設定する
+    ///   5.1 衝突していた場合、移動可能な場所を探索し、書き込む
+    ///   5.2 衝突していた方の元々の位置を初期化する
+    ///   5.3 移動していない方を、起点の場所に書き込む
+    /// 6. 次のlabelの位置を起点にして、2-5を繰り返す
+    ///
+    /// # Arguments
+    /// - `key` - 追加するキー
+    ///
+    /// # Returns
+    /// すでに存在するキーの場合は、そのIDを返す。
+    /// 利用できない文字が含まれている場合は、Errを返す
+    pub fn insert(&mut self, key: &str) -> Result<i32, ()> {
+        let labels = self.labels.key_to_labels(key)?;
+        let mut current = NodeIdx::head();
+
+        for label in labels.iter() {
+            let base = self.nodes[usize::from(current)].base;
+            let transition = base + *label;
+
+            // 対象の場所が空いていればそのままそこに書き込み、空いていなければ衝突しているので解消する
+            self.nodes[usize::from(transition)].check.is_empty();
+
+            // 次の位置はcheckの場所からになる
+            current = transition.into();
+        }
+
+        Ok(i32::from(current))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn it_works() {}
+}
---
+++ trie/src/types.rs
@@ -0,0 +1,390 @@
+use std::{collections::HashMap, ops};
+
+/// 内部で利用するbase/checkのペア
+/// ここで定義されるbase/checkは、内部的には未使用領域を負の値で管理している。
+/// baseにおける負の値は **前の未使用baseのindex** を負の値としており、 checkにおける負の値は、 **次の未使用checkのindex** を負の値としている
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct Node {
+    pub base: Base,
+    pub check: Check,
+}
+
+/// 未使用領域を管理するEMPTYを表す型
+///
+/// 内部で[Rc]を利用しているが、原則としてTrieの生存期間よりも短いため、実際には参照をそのまま保持しても問題ないはず
+pub struct Empties;
+
+/// 未使用領域そのものを与える型
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct Empty(usize);
+
+impl ops::Sub<Label> for Empty {
+    type Output = Option<Base>;
+
+    fn sub(self, rhs: Label) -> Self::Output {
+        if self.0 >= rhs.0 as usize {
+            Some(Base::new((self.0 - rhs.0 as usize) as u32))
+        } else {
+            None
+        }
+    }
+}
+
+impl Empty {
+    fn root() -> Self {
+        Empty(0)
+    }
+
+    fn is_root(&self) -> bool {
+        self.0 == 0
+    }
+}
+
+impl Empties {
+    /// 空配列の集合を返す
+    pub fn as_iter(nodes: &Vec<Node>) -> Box<dyn Iterator<Item = Empty>> {
+        let mut vec: Vec<Empty> = Vec::new();
+
+        let mut root = nodes[0].check.next_empty().unwrap().clone();
+        vec.push(Empty::root());
+
+        while !root.is_root() {
+            vec.push(root.clone());
+
+            if let Some(r) = nodes[root.0].check.next_empty() {
+                root = r
+            } else {
+                break;
+            }
+        }
+
+        unimplemented!("")
+    }
+
+    /// 対象の遷移位置を未使用に変更する
+    pub fn push_unused(vec: &mut Vec<Node>, transiton: Transition) {
+        let base_idx: usize = transiton.into();
+
+        for i in 1..base_idx {
+            if let Some(old) = vec[base_idx - i].check.next_empty() {
+                vec[base_idx - i].check = Check::into_chain(transiton);
+                vec[base_idx].check = Check::from(old);
+                break;
+            }
+        }
+    }
+
+    /// 対象の遷移位置がemptyかどうかを返す
+    pub fn is_empty_at(vec: &Vec<Node>, transition: Transition) -> bool {
+        if let Some(v) = vec.get(usize::from(transition)) {
+            v.check.is_empty()
+        } else {
+            true
+        }
+    }
+
+    /// 未使用領域を `size` だけ広げる
+    ///
+    /// 拡張するというのは未使用領域に閉じている。
+    pub fn expand_empties(vec: &mut Vec<Node>, size: usize) {
+        let current_len = vec.len();
+
+        // 拡張領域を作成する。末尾は常に先頭になる
+        let mut expanded: Vec<Node> = (0..size)
+            .map(|idx| Node {
+                base: Base::empty(),
+                check: Check::empty_at(current_len + idx + 1),
+            })
+            .collect();
+
+        expanded[size - 1].check = Check::root();
+
+        // 現在ある末尾のcheckの値を、今追加する先頭のindexに向ける
+        // 追加するcheckの末尾は、未使用領域の先頭に向けておく
+        // ここではSomeではあるが、実際にはrootの位置が常に未使用になるため、Noneになることはない。
+        if let Some((idx, _)) = vec.iter().enumerate().rfind(|(_, v)| v.check.is_empty()) {
+            vec[idx].check = Check::empty_at(current_len);
+        }
+
+        // 直接追加する
+        vec.extend_from_slice(&expanded)
+    }
+}
+
+/// 内部で利用する値に対して型での表明を行うための型を提供する
+///
+/// このmoduleでは、今回定義するダブル配列において必要になる各種演算のみを許容するように定義している
+
+/// Labelの集合を表す型
+///
+/// charからlabelへの変換も実施するが、
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct Labels(HashMap<char, u8>);
+
+impl Labels {
+    /// labelの集合の数を返す
+    #[inline]
+    pub fn len(&self) -> usize {
+        self.0.len()
+    }
+
+    pub fn from_keys(keys: &Vec<char>) -> Labels {
+        assert!(!keys.is_empty(), "can not accept empty keys");
+        let mut key_map = HashMap::new();
+
+        for (i, key) in keys.iter().enumerate() {
+            key_map.insert(*key, (i + 1) as u8);
+        }
+
+        Self(key_map)
+    }
+
+    /// keyからLabelに変換する
+    ///
+    /// 変換後の順序は、 `key` の文字順と一致する
+    pub fn key_to_labels(&self, key: &str) -> Result<Vec<Label>, ()> {
+        let mut labels: Vec<Label> = Vec::new();
+
+        for c in key.chars() {
+            let id = self.label_of(c)?;
+            labels.push(id);
+        }
+
+        Ok(labels)
+    }
+
+    /// 文字に対応するlabelを返す
+    ///
+    /// # Arguments
+    /// - `c` - 変換する文字
+    ///
+    /// # Returns
+    /// 対応するlabel。labelが存在しないcが渡されるとErr
+    fn label_of(&self, c: char) -> Result<Label, ()> {
+        if let Some(v) = self.0.get(&c) {
+            Ok(Label(*v))
+        } else {
+            Err(())
+        }
+    }
+
+    /// 内部で保持しているラベルの集合を返す
+    pub fn labels(&self) -> Vec<Label> {
+        self.0.values().map(|v| Label(*v)).collect()
+    }
+}
+
+/// Base自体を表す型
+///
+/// Baseはそもそもoffset自体を表現しているので、実際はindexで利用することがほとんどである
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+pub struct Base(i32);
+
+impl Base {
+    /// Baseから利用されうるtransitionの範囲を返す
+    pub fn transition_range(self, labels: &Labels) -> Vec<Transition> {
+        let labels = labels.labels();
+        labels.iter().map(|v| self + *v).collect()
+    }
+
+    /// Baseを新規に作成する
+    pub fn new(v: u32) -> Base {
+        assert!(v > 0, "All bases are should greater than 0");
+        Self(v as i32)
+    }
+
+    /// rootとして扱うBase
+    pub fn root() -> Base {
+        Self(0)
+    }
+
+    /// 未使用のBaseとして扱う
+    pub fn empty() -> Base {
+        Self(-1)
+    }
+
+    /// 使用中であるかを返す
+    pub fn is_used(&self) -> bool {
+        self.0 >= 0
+    }
+
+    /// 未使用であるかを返す
+    pub fn is_empty(&self) -> bool {
+        self.0 < 0
+    }
+}
+
+impl ops::Add<Label> for Base {
+    type Output = Transition;
+
+    fn add(self, rhs: Label) -> Self::Output {
+        assert!(self.0 >= 0, "base should be used");
+        Transition(self.0 as usize + rhs.0 as usize)
+    }
+}
+
+/// Check自体を表す型
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+pub struct Check(i32);
+
+impl Check {
+    /// rootに設定するためのcheckを返す
+    pub fn root() -> Self {
+        Check(-1)
+    }
+
+    /// 内部用のfactory
+    fn empty_at(s: usize) -> Self {
+        Check(-(s as i32 + 1))
+    }
+
+    /// 指定したTransitionを未使用のCheckに変換する
+    ///
+    /// # Returns
+    /// 対象の位置を未使用としてマークした[Check]
+    pub fn into_chain(transition: Transition) -> Self {
+        Self(-(transition.0 as i32 + 1))
+    }
+
+    /// Checkが未使用かどうか返す
+    #[inline]
+    pub fn is_empty(&self) -> bool {
+        self.0 < 0
+    }
+
+    /// Checkが使用中かどうか返す
+    #[inline]
+    pub fn is_used(&self) -> bool {
+        self.0 >= 0
+    }
+
+    /// 次の未使用checkを返す
+    pub fn next_empty(&self) -> Option<Empty> {
+        if self.is_empty() {
+            None
+        } else {
+            Some(Empty((-self.0) as usize - 1))
+        }
+    }
+}
+
+impl From<Check> for Base {
+    /// Baseに変換する
+    fn from(val: Check) -> Self {
+        assert!(val.is_used(), "should be used");
+        Base(val.0)
+    }
+}
+
+impl From<Empty> for Check {
+    fn from(value: Empty) -> Self {
+        Self(-((value.0 + 1) as i32))
+    }
+}
+
+/// Baseから変換する。
+impl From<Base> for Check {
+    fn from(value: Base) -> Self {
+        assert!(value.is_used(), "Can not convert empty base to check");
+        Self(value.0)
+    }
+}
+
+/// labelを表す型
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Ord, PartialOrd)]
+pub struct Label(u8);
+
+impl Label {
+    fn new(v: u8) -> Self {
+        Self(v)
+    }
+}
+
+/// Nodeを指し示すindex
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+pub struct NodeIdx(usize);
+
+impl NodeIdx {
+    /// 先頭を表す[NodeIdx]を返す
+    pub fn head() -> Self {
+        Self(0)
+    }
+}
+
+impl From<NodeIdx> for usize {
+    fn from(value: NodeIdx) -> Self {
+        value.0
+    }
+}
+
+impl From<NodeIdx> for i32 {
+    fn from(value: NodeIdx) -> Self {
+        value.0 as i32
+    }
+}
+
+/// 遷移を表す型
+///
+/// 実際には、Checkを指し示すindex
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+pub struct Transition(usize);
+
+impl Transition {
+    fn is_root(&self) -> bool {
+        self.0 == 0
+    }
+}
+
+impl From<Transition> for NodeIdx {
+    fn from(val: Transition) -> Self {
+        NodeIdx(val.0)
+    }
+}
+
+impl From<Transition> for usize {
+    fn from(value: Transition) -> Self {
+        value.0
+    }
+}
+
+impl ops::Sub<Label> for Transition {
+    type Output = Base;
+
+    fn sub(self, rhs: Label) -> Self::Output {
+        Base((self.0 - rhs.0 as usize) as i32)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    mod base {
+        use crate::types::{Base, Label, Labels};
+
+        #[test]
+        fn root_is_not_empty() {
+            assert!(Base::root().is_used(), "should be used");
+            assert!(!Base::root().is_empty(), "should be used");
+        }
+
+        #[test]
+        #[should_panic]
+        fn should_panic_when_new_base_with_root_index() {
+            Base::new(0);
+        }
+
+        #[test]
+        fn get_transition_range() {
+            // arrange
+            let labels = Labels::from_keys(&vec!['a', 'b']);
+
+            // act
+            let base = Base::new(12);
+            let range = base.transition_range(&labels);
+
+            // assert
+            assert_eq!(range.len(), 2);
+            assert_eq!(range, vec![base + Label::new(1), base + Label::new(2)]);
+        }
+    }
+}
