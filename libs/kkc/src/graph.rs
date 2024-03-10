96135d1148d967109830a35b3e4f06f16e51ae8d
derui <derutakayu@gmail.com>
2024-03-10 11:04:54 +0900

    [wip] feat(kkc): implement graph construction

--- libs/kkc/src/graph.rs
+++ libs/kkc/src/graph.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, VecDeque};
 
 use dic::base::{
     speech::{AffixVariant, Speech},
@@ -11,11 +11,13 @@ use crate::GraphDictionary;
 
 /// 各Nodeにおけるキー
 /// graph内のindexをそのまま指す
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
 struct NodeKey(usize);
 
 /// Graphの中で使われるNode
 ///
 /// BOS/EOS に相当するNodeも含めている
+#[derive(Debug, PartialEq, Clone)]
 enum Node {
     WordNode(Word),
     /// 仮想Nodeに対応する型である。この型は必ずしも存在するものではない
@@ -24,15 +26,7 @@ enum Node {
     EOS,
 }
 
-/// node間を結ぶエッジ
-///
-/// エッジに設定されているコストは、 `from -> to` における遷移コストを表す
-struct Edge {
-    from: NodeKey,
-    to: NodeKey,
-    cost: i32,
-}
-
+#[derive(Debug)]
 pub struct Graph {
     /// graphの中に含まれるNode
     ///
@@ -51,15 +45,16 @@ impl Graph {
     /// # Returns
     /// 構築されたグラフ
     pub fn from_input(input: &str, dic: &GraphDictionary) -> Graph {
-        let mut nodes: Vec<Vec<Node>> = Vec::with_capacity(input.len() + 2);
+        let input = input.chars().collect::<Vec<_>>();
+        let mut nodes: Vec<Vec<Node>> = vec![Default::default(); input.len() + 2];
         nodes[0] = vec![Node::BOS];
         nodes[input.len() + 1] = vec![Node::EOS];
         let mut graph = Graph { nodes };
 
         // まず付属語を検索し、nodesに追加する
-        graph.find_auxiliary(input, dic);
+        graph.find_ancillary(&input, dic);
         // 先頭から始まりうる単語をnodesに追加する
-        graph.find_word_only_first(input, dic);
+        graph.find_word_only_first(&input, dic);
 
         graph
     }
@@ -68,16 +63,16 @@ impl Graph {
     ///
     /// ここでは、あくまで先頭から発見可能である単語に限って検索する。但し、該当する単語が接頭語である場合には
     /// その後ろから再度検索を実施する場合がある。
-    fn find_word_only_first(&mut self, input: &str, dic: &GraphDictionary) {
+    fn find_word_only_first(&mut self, input: &[char], dic: &GraphDictionary) {
         let mut need_re_search_indices: HashSet<usize> = HashSet::new();
 
         for i in 1..input.len() {
-            let key = &input[0..i];
+            let key = input[0..i].iter().collect::<String>();
 
             let words = dic
                 .standard_trie
-                .search(key, &|_, _| {})
-                .and_then(|_| dic.standard_dic.get(key));
+                .search(&key, &|_, _| {})
+                .and_then(|_| dic.standard_dic.get(&key));
 
             if let Some(words) = words {
                 for word in words {
@@ -85,7 +80,7 @@ impl Graph {
                     if word.speech == Speech::Affix(AffixVariant::Prefix) {
                         need_re_search_indices.insert(i + key.len());
                     }
-                    self.nodes[i + key.len()].push(Node::WordNode(word.clone()));
+                    self.nodes[i].push(Node::WordNode(word.clone()));
                 }
             }
         }
@@ -93,16 +88,16 @@ impl Graph {
         // 接頭語があったindexの後ろから単語のみを探索する
         for idx in need_re_search_indices {
             for i in (idx + 1)..input.len() {
-                let key = &input[idx..i];
+                let key = input[idx..i].iter().collect::<String>();
 
                 let words = dic
                     .standard_trie
-                    .search(key, &|_, _| {})
-                    .and_then(|_| dic.standard_dic.get(key));
+                    .search(&key, &|_, _| {})
+                    .and_then(|_| dic.standard_dic.get(&key));
 
                 if let Some(words) = words {
                     for word in words {
-                        self.nodes[i + key.len()].push(Node::WordNode(word.clone()));
+                        self.nodes[i].push(Node::WordNode(word.clone()));
                     }
                 }
             }
@@ -112,17 +107,17 @@ impl Graph {
     /// 付属語を検索して graph のnodeに追加する
     ///
     /// この段階では、仮想ノードへの追加なども実施はせずに、単純にあり得る付属語を検索して追加するのみである
-    fn find_auxiliary(&mut self, input: &str, dic: &GraphDictionary) {
+    fn find_ancillary(&mut self, input: &[char], dic: &GraphDictionary) {
         for i in 0..input.len() {
             let mut j = i;
 
             while j < input.len() {
-                let key = &input[i..j];
+                let key = input[i..j].iter().collect::<String>();
 
                 let words = dic
                     .ancillary_trie
-                    .search(key, &|_, _| {})
-                    .and_then(|_| dic.ancillary_dic.get(key));
+                    .search(&key, &|_, _| {})
+                    .and_then(|_| dic.ancillary_dic.get(&key));
 
                 if let Some(words) = words {
                     for word in words {
@@ -133,5 +128,85 @@ impl Graph {
                 j += 1
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use dic::base::speech::{NounVariant, ParticleType, VerbForm};
+
+    use super::*;
+
+    // テスト用の辞書
+    fn dic() -> GraphDictionary {
+        let keys = vec![
+            'あ', 'い', 'う', 'え', 'お', 'か', 'き', 'く', 'け', 'こ', 'さ', 'し', 'す', 'せ',
+            'そ', 'た', 'ち', 'つ', 'て', 'と', 'な', 'に', 'ぬ', 'ね', 'の', 'は', 'ひ', 'ふ',
+            'へ', 'ほ', 'ま', 'み', 'む', 'め', 'も', 'や', 'ゆ', 'よ', 'ら', 'り', 'る', 'れ',
+            'ろ', 'わ', 'を', 'ん', 'が', 'ぎ', 'ぐ', 'げ', 'ご', 'ざ', 'じ', 'ず', 'ぜ', 'ぞ',
+            'だ', 'ぢ', 'づ', 'で', 'ど', 'ば', 'び', 'ぶ', 'べ', 'ぼ', 'ぱ', 'ぴ', 'ぷ', 'ぺ',
+            'ぽ', 'っ',
+        ];
+        let mut standard_trie = trie::Trie::from_keys(&keys);
+        let mut ancillary_trie = trie::Trie::from_keys(&keys);
+
+        standard_trie.insert("くるま").unwrap();
+        standard_trie.insert("くる").unwrap();
+        ancillary_trie.insert("まで").unwrap();
+        ancillary_trie.insert("で").unwrap();
+
+        GraphDictionary {
+            standard_trie,
+            standard_dic: HashMap::from([
+                (
+                    "くるま".to_string(),
+                    vec![Word::new("くるま", "車", Speech::Noun(NounVariant::Common))],
+                ),
+                (
+                    "くる".to_string(),
+                    vec![Word::new(
+                        "くる",
+                        "来る",
+                        Speech::Verb(VerbForm::Hen("カ".to_string())),
+                    )],
+                ),
+                (
+                    "くる".to_string(),
+                    vec![Word::new(
+                        "くる",
+                        "繰る",
+                        Speech::Verb(VerbForm::Godan("ラ".to_string())),
+                    )],
+                ),
+            ]),
+            ancillary_trie,
+            ancillary_dic: HashMap::from([
+                (
+                    "まで".to_string(),
+                    vec![Word::new(
+                        "まで",
+                        "まで",
+                        Speech::Particle(ParticleType::Adverbial),
+                    )],
+                ),
+                (
+                    "で".to_string(),
+                    vec![Word::new("で", "で", Speech::Particle(ParticleType::Case))],
+                ),
+            ]),
+        }
+    }
+
+    #[test]
+    fn name() {
+        // arrange
+        let dic = dic();
+
+        // act
+        let graph = Graph::from_input("くるまではしらかった", &dic);
+
+        // assert
     }
 }
