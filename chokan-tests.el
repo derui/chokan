e6c6118a2011a4f1a7d82a5bc9fad8fce461afbd
derui <derutakayu@gmail.com>
2024-03-20 16:47:01 +0900

    refactor: change conversion behaviour

--- chokan-tests.el
+++ chokan-tests.el
@@ -22,20 +22,22 @@
 (ert-deftest alphabet-to-kana ()
   (should (equal nil (chokan-roman-table-roman-to-kana "lx")))
   (should (equal nil (chokan-roman-table-roman-to-kana "l")))
-  (should (equal '("し" . "") (chokan-roman-table-roman-to-kana "si")))
-  (should (equal "z"
+  (should (string-equal "し" (chokan-roman-table-roman-to-kana "si")))
+  (should (equal nil
                  (chokan-roman-table-roman-to-kana "z")))
-  (should (equal '("あ" . "")
+  (should (equal "あ"
                  (chokan-roman-table-roman-to-kana "a")))
   (should (equal nil
                  (chokan-roman-table-roman-to-kana "qi")))
   )
 
 (ert-deftest sokuon-to-kana ()
-  (should (equal '("っ" . "t")
+  (should (equal nil
                  (chokan-roman-table-roman-to-kana "tt")))
-  (should (equal '("っ" . "s")
-                 (chokan-roman-table-roman-to-kana "ss")))
+  (should (equal "っす"
+                 (chokan-roman-table-roman-to-kana "ssu")))
+  (should (equal "っった"
+                 (chokan-roman-table-roman-to-kana "ttta")))
   )
 
 (ert-deftest hira-to-kata ()
