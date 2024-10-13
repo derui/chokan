e58e537ce6dd7aafb1da963b3a4d9dc89d63199e
derui <derutakayu@gmail.com>
2024-10-13 17:26:38 +0900

    chore: add flake to develop with nix

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "chokan - chotto kanzen";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    # Rust tool management
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, fenix }:let
+    overlays = [
+      fenix.overlays.default
+    ];
+
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+      inherit overlays;
+    };
+  in {
+
+    packages.x86_64-linux.default = pkgs;
+
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      buildInputs = [
+        (pkgs.fenix.stable.withComponents [
+          "cargo"
+          "clippy"
+          "rust-src"
+          "rustc"
+          "rustfmt"
+        ])
+        pkgs.rust-analyzer-nightly
+        pkgs.gcc
+        pkgs.pre-commit
+      ];
+
+      shelHook = ''
+ pre-commit install
+      '';
+    };
+  };
+}
